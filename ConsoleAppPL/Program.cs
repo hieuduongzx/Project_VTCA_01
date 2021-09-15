0dd2446409e197c0527682ff204c738801e6e092
hieuduongzx <hieuduongzx@gmail.com>
2021-09-15 15:28:35 +0700

    push

---
+++ ConsoleAppPL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User Name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Password: ");
+            Console.WriteLine();
+        }
+    }
+}
